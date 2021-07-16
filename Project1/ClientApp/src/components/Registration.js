ddf9a215bb257437539919783ef1f889af897b77
TinMoe LinTun <tinmoelintun@hotmail.com>
2021-07-16 22:22:25 +1200

    Added Registration

---
+++ Project1/ClientApp/src/components/Registration.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class Registration extends Component {
+    static displayName = Registration.name;
+
+    render() {
+        return (
+            <div>
+                <h1>Registration</h1>
+            </div>
+        );
+    }
+}
---
+++ Project1/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CreditCardNo { get; set; }
+        public int CVC { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
